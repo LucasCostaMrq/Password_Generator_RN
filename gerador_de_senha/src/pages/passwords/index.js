ef5e77d989563640e5cdc86a84167cdf3608be4e
Lucas Costa Marques <124885548+LucasCostaMrq@users.noreply.github.com>
2024-05-05 16:14:47 -0300

    Add files via upload

--- gerador_de_senha/src/pages/passwords/index.js
+++ gerador_de_senha/src/pages/passwords/index.js
@@ -1,28 +1,48 @@
-import { View, Text, StyleSheet} from 'react-native'
+import { View, Text, StyleSheet, FlatList} from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useState, useEffect } from 'react'
 import { useIsFocused } from '@react-navigation/native'
 import useStorage from '../../hooks/useStorage'
+import { PasswordItem } from './components/passItem'
 
 export function Passwords(){
     const [listPasswords, setListPasswords] = useState([])
     const focused = useIsFocused()
-    const { getItem } = useStorage()
+    const { getItem, removeItem } = useStorage()
 
     useEffect (()=>{
         async function loadPasswords(){
-            const passwords = getItem("@pass")
-            console.log(passwords)
+            const passwords = await getItem("@pass")
+            setListPasswords(passwords)
         }
         loadPasswords()
     }, [focused])
+
+    async function deletePassword(item){
+        const passwords = await removeItem("@pass", item)
+        setListPasswords(passwords)
+    }
+    
     return(
         <SafeAreaView style={{flex:1}}>
             <View style={styles.header}>
                 <Text style={styles.title}>Minhas senhas</Text>
             </View>
+
+            <View style={styles.content}>
+                <FlatList
+                    style={{flex: 1, paddingTop: 14}}
+                    data={listPasswords}
+                    keyExtractor= {(item)=>String(item)}
+                    renderItem={({item}) => <PasswordItem data={item} removePassword={()=> deletePassword(item)}/>}
+                />
+            </View>
         </SafeAreaView>
     )
+    //FlatList=> irá renderizar as buscas de listas do usoário
+        // data: valor da lista que será renderizada
+        // keyExtractor: identificador de cada item da lista; cada senha é unica
+        // renderItem: renderiza os valores de cada lista unicamente
 }
 
 const styles = StyleSheet.create({
@@ -39,5 +59,11 @@ const styles = StyleSheet.create({
         fontSize: 20,
         fontWeight: "600",
         color: "#FFF"
+    },
+
+    content:{
+        flex: 1,
+        paddingLeft: 14,
+        paddingRight: 14,
     }
 })
